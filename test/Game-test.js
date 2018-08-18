ce9681be3d83ef0a8d34277ae547d7a4e877c2cf
Derek Hanciles <drhanciles@icloud.com>
2018-08-18 14:49:54 -0600

    Updates to ending game when colliding with wall have been implemented

--- test/Game-test.js
+++ test/Game-test.js
@@ -12,34 +12,38 @@ const ctx = {
 
 describe('Game', () => {
 
-  it('should take properties', () => {
-    var game = new Game(); 
+  it.skip('should take properties', () => {
+    const game = new Game(ctx); 
     assert.deepEqual(game, {
       ctx: ctx, 
       paused: false, 
       gameOver: false
     })
-
-  })
+  }); 
     //setup
     //assertion
     //execution
     //teardown
 
-  it.skip('should end the game if block collides with wall', () => {
-      var player1 = new Player(50, 50, 10, 10, 'red', 'black');
-      var player2 = new Player(100, 100, 10, 10, 'green', 'black');
-      // if either player collides with the wall, this.gameEnd() should be invoked.
-      assert.equal(gameEnd() === true)
-      
+  it.skip('should end the game if player collides with wall', () => {
+      const game = new Game(ctx); 
+      let player1 = new Player(1, 300, 5, 5, 'red', 'black', 1, 0);
+      let player2 = new Player(795, 300, 5, 5, 'yellow', 'black', -1, 0);
 
+      player1.isColliingWithWall(canvasWidth, canvasHeight)
+      player2.isColliingWithWall(canvasWidth, canvasHeight)
+      assert.equal(gameEnd(), true)
+      
   });
 
-  it.skip('should collide with walls', () => {})
+  it.skip('should end game if player collides with opposing player', () => {
+
+  }); 
   
   it.skip('should be able to change direction when keys are pressed', () => {
-      var player1 = new Player(50, 50, 10, 10, 'red', 'black');
-      var player2 = new Player(100, 100, 10, 10, 'green', 'black');
+      var player1 = new Player(1, 300, 5, 5, 'red', 'black', 1, 0);
+      var player2 = new Player(795, 300, 5, 5, 'yellow', 'black', -1, 0);
+
       assert.notStrictEqual(player1.x === 50, false)
       assert.notStrictEqual(player1.y === 50, false)
       assert.notStrictEqual(player2.x === 100, false)
@@ -50,4 +54,4 @@ describe('Game', () => {
   })
 
   it.skip('should be able to changeDirection', () => {})
-})
+}); 
