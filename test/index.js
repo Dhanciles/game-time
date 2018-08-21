3022d757723ed85eb4fb184258f96ece2983814b
Cierra Washington <cierrajwashi@gmail.com>
2018-08-21 09:56:47 -0600

    saving ui changes

---
+++ test/index.js
@@ -0,0 +1,37 @@
+// const Game = require('./Game');
+
+// const canvas = document.querySelector('#game');
+// const ctx = canvas.getContext('2d');
+// const game = new Game(ctx);
+
+// $('#tron-button').on('click', startGame);
+
+// function startGame(){
+
+//   window.requestAnimationFrame(gameLoop);
+
+//   console.log('animation requested!!');
+
+// }
+
+// function gameLoop () {
+
+//   if (game.isOver()) {
+//     console.log('Game Over');
+
+//   } else {
+//     // clear previous frame
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+//     // draw this frame
+//     game.animate();
+//   }
+
+// }
+
+// // Add key press event handler
+// document.addEventListener('keydown', handleKeyPress);
+
+// function handleKeyPress(e) {
+//   game.handleKeyPress(e);
+// }
