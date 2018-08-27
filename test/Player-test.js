cf8d1de28ee7c56e6db1011f5ad581aa3b9142a7
Derek Hanciles <drhanciles@icloud.com>
2018-08-26 23:09:11 -0600

    All player-test are now running and working properly

--- test/Player-test.js
+++ test/Player-test.js
@@ -4,37 +4,21 @@ const Game = require('../lib/Game');
 
 describe('Player', function() {
   let player;
-  let game = new Game(ctx);
-  let ctx = game.ctx;
-
-  beforeEach(() => {
-    let player1 = new Player (1, 300, 5, 5, 'red', 'black', 1, 0, 3); 
-    let player2 = new PLayer (795, 300, 5, 5, 'yellow', 'black', -1, 0, 3);
-  });
   
-  class Context {
-    constructor() {
-      this.canvas = 'canvas#game';
-      this.fillStyle = '#000000'; 
-    }
-  }; 
+  
+  it('should instansitate a new player', () => {
+    let player = new Player(1, 300, 5, 5, 'red', 'black', 1, 0, 3); 
 
-  it.skip('should take properties', () => {
-    assert.deepEqual(player, {
-      x: 30,
-      y: 30,
-      height: 10,
-      width: 10,
-      color: color,
-      borderColor: borderColor,
-      dx: 1,
-      dy: 0,
-    });
-  });
+    assert.isObject(player)
+  }); 
 
+  it('should take properties', () => {
+    let player = new Player(1, 300, 5, 5, 'red', 'black', 1, 0, 3); 
 
-  it.skip('should instantiate a new player', function() {
-    let player1 = new Player (1, 300, 5, 5, 'red', 'black', 1, 0, 3); 
-    let player2 = new Player (795, 300, 5, 5, 'yellow', 'black', -1, 0, 3);
-  });
-})
+    assert.equal(player.x, 1); 
+    assert.equal(player.y, 300); 
+    assert.equal(player.height, 5); 
+    assert.equal(player.width, 5);
+    assert.equal(player.lives, 3);  
+    }); 
+}); 
